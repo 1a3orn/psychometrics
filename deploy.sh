295fc7f8f22a7be5483cd58f5bd34e493626dd8c
1a3orn <1a3orn@protonmail.com>
2024-09-12 17:47:56 -0400

    Changes.

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@
 git pull origin main
 
 # Build and start containers
-docker-compose -f docker-compose.prod.yml up --build -d
+# while adding the .env.prod file env variables
+export $(grep -v '^#' .env.prod | xargs) && docker-compose -f docker-compose.prod.yml up --build -d
 
 # Run migrations
-docker-compose -f docker-compose.prod.yml run --rm migration
+export $(grep -v '^#' .env.prod | xargs) && docker-compose -f docker-compose.prod.yml run --rm migration
 
 # Remove unused images
 docker image prune -f
